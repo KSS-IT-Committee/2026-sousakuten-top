3c1c8cfd5c5e497129a0b0f3dc07bcc17ee05e1b
K10-K10 <keito.tottori@gmail.com>
2026-09-06 00:24:50 +0900

    feat: add club schedule

--- lib/schedule.ts
+++ lib/schedule.ts
@@ -30,3 +30,80 @@ export const RISSI_PERFORMANCE = {
   AM: "09:00 - 11:25",
   PM: "12:25 - 15:30",
 };
+
+const BRASS_BAND_PERFORMANCE = {
+  1: "10:55 - 11:35",
+  2: "12:30 - 13:00",
+  3: "14:20 - 15:00",
+};
+
+const SADOU_PERFORMANCE = {
+  第1公演: "09:00 - 09:20",
+  第2公演: "09:30 - 09:50",
+  第3公演: "10:00 - 10:20",
+  第4公演: "10:30 - 10:50",
+  第5公演: "11:00 - 11:20",
+  第6公演: "12:30 - 12:50",
+  第7公演: "13:00 - 13:20",
+  第8公演: "13:30 - 13:50",
+  第9公演: "14:00 - 14:20",
+  第10公演: "14:30 - 14:50",
+};
+
+const PHILHARMONIC_PERFORMANCE = {
+  第1公演: "09:00 - 11:20",
+  第2公演: "12:30 - 15:15",
+};
+
+const KARUTA_PERFORMANCE = {
+  "模擬試合 1": "09:00 - 10:00",
+  "模擬試合 2": "10:25 - 11:25",
+  かるた体験: "12:30 - 15:00",
+};
+
+const QUIZ_PERFORMANCE = {
+  クイズ体験: "09:00-11:30",
+  ミニクイズ大会: "13:55 - 14:30",
+};
+
+const COOKING_PERFORMANCE = {
+  お菓子販売: "12:45 - 14:00",
+};
+
+const CHORUS_PERFORMANCE = {
+  第1公演: "09:00 - 09:30",
+  第2公演: "13:30 - 14:00",
+};
+
+const THEATER_PERFORMANCE = {
+  第1公演: "08:55 - 09:50",
+  第2公演: "10:30 - 11:25",
+  第3公演: "12:40 - 13:35",
+  第4公演: "14:15 - 15:10",
+};
+
+const JAZZ_PERFORMANCE = {
+  "ジャズライブ 1": "10:30 - 11:30",
+  "ジャズライブ 2": "13:00 - 14:00",
+};
+
+const CHEMISTRY_PERFORMANCE = {
+  実験ショー1: "09:00 - 09:20",
+  実験ショー2: "11:00 - 11:20",
+  実験ショー3: "12:30 - 12:50",
+  実験ショー4: "13:40 - 14:00",
+  実験ショー5: "14:50 - 15:10",
+};
+
+export const clubPerformanceTime = {
+  BRASS_BAND: [BRASS_BAND_PERFORMANCE, "09/12(土)", "剣道場"],
+  SADOU: [SADOU_PERFORMANCE, "09/12(土) 09/13(日)", "茶室"],
+  PHILHARMONIC: [PHILHARMONIC_PERFORMANCE, "09/13(土)", "剣道場"],
+  KARUTA: [KARUTA_PERFORMANCE, "09/12(土) ", "柔道場"],
+  QUIZ: [QUIZ_PERFORMANCE, "09/12(土) 09/13(日)", "視聴覚室"],
+  COOKING: [COOKING_PERFORMANCE, "09/12(土) 09/13(日)", "調理室・被覆室"],
+  CHORUS: [CHORUS_PERFORMANCE, "09/12(土))", "音楽室"],
+  THEATER: [THEATER_PERFORMANCE, "09/12(土) 09/13(日)", "401教室"],
+  JAZZ: [JAZZ_PERFORMANCE, "09/13(日)", "音楽室"],
+  CHEMISTRY: [CHEMISTRY_PERFORMANCE, "09/12(土) 09/13(日)", "化学室"],
+};
