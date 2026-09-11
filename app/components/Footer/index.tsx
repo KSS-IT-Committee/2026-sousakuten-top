e7369df7d9b65c54dd6adff708774e7a8264cd53
K10-K10 <keito.tottori@gmail.com>
2026-09-11 23:42:52 +0900

    feat:add questionnaire link about site evalution

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -7,6 +7,14 @@ export function Footer() {
         <span>行事週間2026</span>
         <span>青、薫る</span>
       </div>
+      <a
+        className={styles.feedbackLink}
+        href="https://forms.cloud.microsoft/r/zm0he7sDxz"
+        target="_blank"
+        rel="noreferrer"
+      >
+        サイトの感想フォーム
+      </a>
       <div>
         <span>© 2026 東京都立小石川中等教育学校</span>
         <span>行事運営委員会・IT委員会</span>
